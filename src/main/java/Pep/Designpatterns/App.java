2ff7f339ff4c26f2c080ed1d8a4aa99d5e35c840
sreeja099 <69036662+sreeja099@users.noreply.github.com>
2020-07-31 16:24:40 +0530

    Add files via upload

---
+++ src/main/java/Pep/Designpatterns/App.java
@@ -0,0 +1,29 @@
+package Pep.Designpatterns;
+
+public class App
+{
+	private static App cal ;
+	private App() {};
+	public static App getInstance()
+	{
+		if(cal==null)
+			cal = new App();
+		return cal;
+	}
+	public int addition(int a,int b)
+	{
+		return a+b;
+	}
+	public  int subtraction(int a,int b)
+	{
+		return a-b;
+	}
+	public int multiplication(int a,int b)
+	{
+		return a*b;
+	}
+	public int division(int a,int b)
+	{
+		return a/b;
+	}
+}
